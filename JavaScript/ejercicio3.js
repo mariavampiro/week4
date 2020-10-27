25b9888813cf12eb1075bc8adb03d8694f8367e9
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2020-10-26 23:37:56 -0300

    ex 3

--- JavaScript/ejercicio3.js
+++ JavaScript/ejercicio3.js
@@ -2,14 +2,38 @@
 
 //a
 
+let arr=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+let month5=arr[4];
+let month11=arr[10];
+console.log(month5,month11);
+
 //b
+
+//console.log(arr.sort());
 
 //c
 
+arr.unshift('hello');
+arr.push('world');
+console.log(arr);
+
 //d
+
+arr.shift();
+arr.pop();
+console.log(arr);
 
 //e
 
+//arr.reverse();
+console.log(arr);
+
 //f
 
+var stjoin=arr.join('-');
+console.log(stjoin);
+
 //g
+
+let arr2=arr.slice(4,10);
+console.log(arr2);
