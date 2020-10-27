43887d02789977fd13076d02ecef5b6421dcdba6
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2020-10-26 21:49:58 -0300

    ex 2

--- JavaScript/ejercicio2.js
+++ JavaScript/ejercicio2.js
@@ -2,13 +2,33 @@
 
 //a
 
+var st1='mynameismaria';
+console.log(st1.toUpperCase())
+
 //b
+
+var st2='mynameismaria';
+var st3=st2.substring(5,0);
+console.log(st3);
 
 //c
 
+var st4=st2.substring(10,13);
+console.log(st4);
+
 //d
+
+var st5=st2.substring(0,1).toUpperCase()+st2.substring(1,13);
+console.log(st5);
 
 //e
 
+var st6='hello world my name is maria'
+var position=st6.indexOf(' ');
+console.log(position);
+
 //f
 
+var st7='problematica ejercicios';
+var st8=st7.substring(0,1).toUpperCase()+st7.substring(1,13)+st7.substring(13,14).toUpperCase()+st7.substring(14,22);
+console.log(st8);
