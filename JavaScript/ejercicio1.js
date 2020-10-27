9412109b6622573e5286edc84b95f63e39c75661
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2020-10-26 21:17:24 -0300

    ex 1

--- JavaScript/ejercicio1.js
+++ JavaScript/ejercicio1.js
@@ -9,7 +9,17 @@ console.log('La suma es ' +suma);
 
 //b
 
+var st1='hello';
+var st2='world';
+var st3=st1+st2;
+console.log(st3);
+
 //c
+
+var name='maria';
+var surname='vampiro';
+var sum=name.length+surname.length;
+console.log(sum);
 
 
 
