eb0664726f944643edd5b157a78194a091fb996b
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2020-10-27 23:07:55 -0300

    corrections

--- JavaScript/ejercicio6.js
+++ JavaScript/ejercicio6.js
@@ -6,40 +6,53 @@ function suma(a,b){
     return console.log(a+b);
 }
 
-//b
 
-function validacion(){
-    if (isNaN(a)){
+//b  A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, 
+//   mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+
+function validacion(a,b){
+    if (isNaN(a)||isNaN(b)){
         alert('Uno de los parametros tiene error');
-        console.log(a);
-    }else if(isNaN(b)){
-        alert('Uno de los parametros tiene error');
-        console.log(b);
+        if(isNaN(a)){
+            alert(a);
+        }else{
+            alert(b);
+        }
     }
 }
 
-//c
+//c Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 
 function validateinteger(num1){
-    if(Number.isInteger()){
-        return true;
+    return Number.isInteger(num1)
+}
+
+//d   A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. 
+//    En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
+
+function validar(a,b){
+    if(Number.isInteger(a)){
+    }else{
+        alert('Error');
+        alert(Math.round(a));
+    }
+    if(Number.isInteger(b)){
+    }else{
+        alert('Error');
+        alert(Math.round(b));
     }
 }
 
-//d
+//e   Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma
+//    probando que todo siga funcionando igual.
 
-function validar(a,b){
-    if(Number.isInteger())
-}else if{
-    alert('Error');
-    alert(Math.round(Number));
-}
-
-//e
-
-function validar(a,b){
-    if(Number.isInteger())
-}else if{
-    alert('Error');
-    alert(Math.round(Number));
+function validacion(a,b){
+    if (isNaN(a)||isNaN(b)){
+        alert('Uno de los parametros tiene error');
+        if(isNaN(a)){
+            alert(a);
+        }else{
+            alert(b);
+        }
+    }
 }
